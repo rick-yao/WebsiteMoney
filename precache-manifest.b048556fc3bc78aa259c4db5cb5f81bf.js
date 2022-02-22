1507d140eb7beda52ffe16e2695d12ac888a6584
rick <abneryaoo@gmail.com>
2022-02-22 11:50:40 +0800

    deploy

---
+++ precache-manifest.b048556fc3bc78aa259c4db5cb5f81bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d54e5c7b8acf7d6cb601",
+    "url": "/WebsiteMoney/css/app.bd5de690.css"
+  },
+  {
+    "revision": "9c3214d8df402c98f8b50eee9f9253a0",
+    "url": "/WebsiteMoney/index.html"
+  },
+  {
+    "revision": "d54e5c7b8acf7d6cb601",
+    "url": "/WebsiteMoney/js/app.b74eebcc.js"
+  },
+  {
+    "revision": "1d116c49e13d4c5e6eb6",
+    "url": "/WebsiteMoney/js/chunk-vendors.30102d75.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/WebsiteMoney/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/WebsiteMoney/robots.txt"
+  }
+]);
